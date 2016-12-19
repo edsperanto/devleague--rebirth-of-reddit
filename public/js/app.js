24f697daf9cfdb67ae0c79857ca11c29c523e16d
Edward Gao <egao@sent.com>
2016-12-19 09:51:00 -1000

    reddituploads now works

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ function load(link, callback) {
 }
 
 function notFindStr(searchQuery) {
-	let blackList = ['imageflake.com', 'i.reddituploads.com', 'thnk1994.com', 'youtube.com', 'flickr', '/a/', '/gallery/', '/r/'];
+	let blackList = ['imageflake.com'/*, 'i.reddituploads.com'*/, 'thnk1994.com', 'youtube.com', 'youtu.be', 'docs.google.com', 'flickr', 'instagram', '500px', 'pinkbike', '/a/', '/gallery/', '/r/'];
 	for(let i = 0; i < blackList.length; i++) {
 		if(searchQuery.indexOf(blackList[i]) > -1) { return false; }
 	}
@@ -14,7 +14,7 @@ function notFindStr(searchQuery) {
 }
 
 function findExt(searchQuery) {
-	let whiteList = ['.jpg', '.gifv', '.gif', '.png', 'shite.xyz', '.jpeg'];
+	let whiteList = ['.jpg', '.gifv', '.gif', '.png', 'shite.xyz', '.jpeg', 'reddituploads'];
 	for(let i = 0; i < whiteList.length; i++) {
 		if(searchQuery.indexOf(whiteList[i]) > -1) { return true; }
 	}
@@ -29,7 +29,7 @@ function loadSub(name) {
 		for(let i = 0; i < response.length; i++) {
 			let pass = 	(notFindStr(response[i].data.url)) &&
 						(typeof response[i].data.preview === 'object');
-			if(pass) { getContent(i); }
+			if(pass || response[i].data.domain === 'i.reddituploads.com') { getContent(i); }
 		}
 		function getContent(num) {
 			let post = document.createElement('div');
@@ -54,7 +54,7 @@ function loadSub(name) {
 			stats.className = 'post-stats';
 			snippet.className = 'post-snippet';
 			imageLink.href = imgHref;
-			image.style.backgroundImage = `url(${imgHref})`;
+			image.style.backgroundImage = `url(${previewHref})`;
 			content.appendChild(post);
 			post.appendChild(aspect);
 			post.appendChild(postContent);
@@ -62,24 +62,40 @@ function loadSub(name) {
 			imageLink.appendChild(image);
 		}
 		function getPreview(response, num) {
-			let images = response[num].data.preview.images[0];
-			if(response[num].data.url.indexOf('.gif') > -1) {
-				console.log(images);
-				let gifs = images.variants.gif.resolutions;
-				return gifs[gifs.length - 1];
+			if(response[num].data.domain === 'i.reddituploads.com') {
+				let tempStr = "";
+				for(let i = 0; i < response[num].data.url.length; i++) {
+					if(response[num].data.url.charAt(i) === '&') {
+						tempStr += '\&';
+						i += 4;
+					}else{
+						tempStr += response[num].data.url.charAt(i);
+					}
+				}
+				return tempStr;
 			}else{
-				console.log(images);
-				return images.resolutions[images.resolutions.length - 1];
+				let images = response[num].data.preview.images[0];
+				if(response[num].data.url.indexOf('.gif') > -1) {
+					let gifs = images.variants.gif.resolutions;
+					return gifs[0]; // high-def: gifs.length - 1
+				}else{
+					return images.resolutions[0]; // high-def: images.resolutions.length - 1
+				}
 			}
 		}
 	});
 }
 
 function loadRndSub() {
-	let subreddits = ['cats', 'aww', 'EarthPorn', 'scenery', 'AccidentalRenaissance', 'ProgrammerHumor', 'softwaregore', 'spaceporn', 'techsupportgore', 'firstworldanarchists']
-	loadSub(subreddits[Math.floor(Math.random() * subreddits.length)]);
+	let subreddits = ['cats', 'aww', 'EarthPorn', 'auroraporn', 'softwaregore', 'spaceporn', 'foodporn', 'grilledcheese', 'techsupportgore', 'firstworldanarchists']
+	loadSub(subreddits[Math.floor(Math.random() * (subreddits.length - 1))]);
 }
 
 document.getElementById('random').addEventListener('click', loadRndSub);
+document.getElementById('app').addEventListener('click', function() {
+	alert('You are here!')
+});
 
-loadRndSub();
+//loadRndSub();
+
+loadSub('scenery');
